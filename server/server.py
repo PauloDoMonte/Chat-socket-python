7bdf48d1034d4c2bff65e20816e680fb34c5d3cb
paulodomonte <paulo.monte.fis@gmail.com>
2024-11-12 00:53:09 -0300

    Melhorias

--- server/server.py
+++ server/server.py
@@ -12,18 +12,23 @@ def main():
     and manages client interactions.
     """
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((SERVER_IP, SERVER_PORT))
-    server.listen(5)
-    print(f"[STARTING] Server started on {SERVER_IP}:{SERVER_PORT}")
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    try:
+        server.bind((SERVER_IP, SERVER_PORT))
+        server.listen(5)
+        print(f"[STARTING] Server started on {SERVER_IP}:{SERVER_PORT}")
 
-    threading.Thread(target=main_menu, daemon=True).start()
+        threading.Thread(target=main_menu, daemon=True).start()
 
-    client_manager = ClientManager()
+        client_manager = ClientManager()
 
-    while True:
-        try:
-            client_socket, client_address = server.accept()
-            threading.Thread(target=client_manager.handle_client, args=(client_socket, client_address), daemon=True).start()
-        except KeyboardInterrupt:
-            print("\n[SERVER] Shutting down...")
-            break
+        while True:
+            try:
+                client_socket, client_address = server.accept()
+                threading.Thread(target=client_manager.handle_client, args=(client_socket, client_address), daemon=True).start()
+            except KeyboardInterrupt:
+                print("\n[SERVER] Shutting down...")
+                break
+    finally:
+        server.close()
